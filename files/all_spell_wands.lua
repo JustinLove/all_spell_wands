c10c9d1a8c52eabb3502cd7361321beef940ad10
Justin Love <git@JustinLove.name>
2021-12-14 19:56:46 -0600

    semistructured no shuffles

--- files/all_spell_wands.lua
+++ files/all_spell_wands.lua
@@ -1,4 +1,17 @@
+dofile_once("mods/all_spell_wands/files/spell_metadata.lua")
+
+function asw_random_modifier( level )
+	return asw_spells_which_draw[Random(1, #asw_spells_which_draw)]
+end
+
+function asw_random_terminal( level )
+	return asw_spells_terminal[Random(1, #asw_spells_terminal)]
+end
+
+asw_setup_spell_draw()
+
 function asw_wand_add_random_cards( gun, entity_id, level )
+
 	local is_rare = gun["is_rare"]
 	local x, y = EntityGetTransform( entity_id )
 	if( level == nil ) then level = 1 end
@@ -7,6 +20,7 @@ function asw_wand_add_random_cards( gun, entity_id, level )
 	level = level - 1
 	local deck_capacity = gun["deck_capacity"]
 	local actions_per_round = gun["actions_per_round"]
+	local shuffle = gun["shuffle_deck_when_empty"]
 	local card_count = Random( 0.51 * deck_capacity, deck_capacity )
 	card_count = clamp( card_count, 1, deck_capacity-1 )
 
@@ -15,22 +29,27 @@ function asw_wand_add_random_cards( gun, entity_id, level )
 	end
 
 	local bullet_card
-	--card = GetRandomActionWithType( x, y, level, ACTION_TYPE_PROJECTILE )
 	card = "LIGHT"
-	for i = 1, card_count-1 do
-		card = GetRandomAction( x, y, level, i )
-		AddGunAction( entity_id, card )
+
+	if shuffle == 1 then
+		for i = 1, card_count-1 do
+			card = GetRandomAction( x, y, level, i )
+			AddGunAction( entity_id, card )
+		end
+	else
+		for i = 1, card_count-1 do
+			if Random(1, card_count) <= actions_per_round then
+				card = asw_random_terminal( level )
+			else
+				card = asw_random_modifier( level )
+				actions_per_round = actions_per_round + asw_spell_draw[card]
+			end
+			actions_per_round = actions_per_round - 1
+			AddGunAction( entity_id, card )
+		end
 	end
 
-	if Random(0,1) == 1 then
-		card = GetRandomActionWithType( x, y, level, ACTION_TYPE_PROJECTILE )
-	elseif Random(0,1) == 1 then
-		card = GetRandomActionWithType( x, y, level, ACTION_TYPE_STATIC_PROJECTILE )
-	elseif Random(0,1) == 1 then
-		card = GetRandomActionWithType( x, y, level, ACTION_TYPE_STATIC_MATERIAL )
-	else
-		card = GetRandomActionWithType( x, y, level, ACTION_TYPE_PASSIVE)
-	end
+	card = asw_random_terminal( level )
 	AddGunAction( entity_id, card )
 end
 
@@ -43,4 +62,9 @@ function asw_wand_add_always_cast( gun, entity_id, level )
 		card = GetRandomAction( x, y, level, i )
 	end
 	AddGunActionPermanent( entity_id, card )
+end
+
+function asw_print_table( t )
+	dofile_once( "data/scripts/lib/utilities.lua" )
+	debug_print_table( t )
 end
---
+++ files/spell_metadata.lua
@@ -0,0 +1,90 @@
+dofile_once("data/scripts/gun/gun_actions.lua")
+dofile_once("data/scripts/gun/gunaction_generated.lua")
+
+ACTION_DRAW_RELOAD_TIME_INCREASE = 0
+
+asw_spell_draw = nil
+asw_spells_which_draw = {}
+asw_spells_terminal = {}
+current_reload_time = 0
+c = {}
+shot_effects = {}
+discarded = { }
+deck = { }
+hand = { }
+mana = 0.0
+
+local function register_draw( action, c )
+	if c == nil then
+		return
+	end
+	asw_spell_draw[action.id] = c.action_draw_many_count
+	--print( tostring(action.id) .. " " .. tostring(c.action_draw_many_count) )
+	if c.action_draw_many_count > 0 then
+		table.insert(asw_spells_which_draw, action.id)
+	else
+		table.insert(asw_spells_terminal, action.id)
+	end
+end
+
+function Reflection_RegisterProjectile( entity_filename )
+end
+
+function add_projectile( entity_filename )
+end
+
+function add_projectile_trigger_timer( entity_filename, delay_frames, action_draw_count )
+	c.action_draw_many_count = action_draw_count
+end
+
+function add_projectile_trigger_hit_world( entity_filename, action_draw_count )
+	c.action_draw_many_count = action_draw_count
+end
+
+function add_projectile_trigger_death( entity_filename, action_draw_count )
+	c.action_draw_many_count = action_draw_count
+end
+
+function draw_actions( how_many, instant_reload_if_empty )
+	c.action_draw_many_count = how_many
+end
+
+function check_recursion( data, rec_ )
+	return -1
+end
+
+function create_shot_state()
+	local value = {}
+	ConfigGunActionInfo_Init(value)
+	return value
+end
+
+function asw_setup_spell_draw() 
+	if asw_spell_draw ~= nil then
+		return
+	end
+
+	asw_spell_draw = {}
+	asw_spells_which_draw = {}
+	asw_spells_terminal = {}
+
+	reflecting = true
+	local asw_EntityLoad = EntityLoad
+	EntityLoad = function() end
+
+	for i,action in ipairs(actions) do
+		current_reload_time = 0
+		c = create_shot_state()
+		shot_effects = {recoil_knockback = 0}
+		discarded = { }
+		deck = { }
+		hand = { }
+		mana = 0.0
+		action.action()
+		register_draw( action, c )
+	end
+
+	reflecting = false
+	EntityLoad = asw_EntityLoad
+end
+
